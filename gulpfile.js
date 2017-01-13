04f13df3cfe05773d2989a0806f1ff6a9b65c2d3
simoneas02 <simoneas02@hotmail.com>
2017-01-13 01:14:21 -0300

    add basic tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const stylus = require('gulp-stylus');
+const connect = require('gulp-connect');
 
-gulp.task('server', () => {
-  console.log("simone");
+gulp.task('pug', () =>{
+  gulp.src('./src/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./out/'))
+    .pipe(connect.reload());
 });
+
+gulp.task('stylus', () =>{
+  gulp.src('./src/assets/styles/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./out/assets/styles/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', () =>{
+  gulp.watch(['./src/*.pug','./src/**/*.pug'], ['pug']);
+  gulp.watch(['./src/assets/styles/*.styl'], ['stylus']);
+});
+
+gulp.task('server', () =>{
+  connect.server({
+    root: './out/',
+    livereload: true
+  });
+});
+
+gulp.task('default', ['watch','server']);
