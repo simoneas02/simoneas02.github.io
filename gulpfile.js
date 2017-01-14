9326d70de057599db2eab08c052e2b6ebac4ca25
simoneas02 <simoneas02@hotmail.com>
2017-01-14 02:46:31 -0300

    add basic imagemin task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 const stylus = require('gulp-stylus');
 const connect = require('gulp-connect');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('pug', () =>{
   gulp.src('./src/*.pug')
@@ -12,14 +13,28 @@ gulp.task('pug', () =>{
 
 gulp.task('stylus', () =>{
   gulp.src('./src/assets/styles/*.styl')
-    .pipe(stylus())
+    .pipe(stylus({
+      compress: true
+    }))
     .pipe(gulp.dest('./out/assets/styles/'))
     .pipe(connect.reload());
 });
 
+gulp.task('imagemin', () =>{
+  gulp.src('./src/assets/img/*')
+    .pipe(imagemin(
+      [imagemin.gifsicle(), imagemin.jpegtran(), imagemin.optipng(), imagemin.svgo()]
+    ))
+    .pipe(gulp.dest('./out/assets/img/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('build', ['pug', 'stylus', 'imagemin']);
+
 gulp.task('watch', () =>{
   gulp.watch(['./src/*.pug','./src/**/*.pug'], ['pug']);
   gulp.watch(['./src/assets/styles/*.styl'], ['stylus']);
+  gulp.watch(['./src/assets/img/*'], ['imagemin']);
 });
 
 gulp.task('server', () =>{
@@ -29,4 +44,6 @@ gulp.task('server', () =>{
   });
 });
 
-gulp.task('default', ['watch','server']);
+
+
+gulp.task('serve', ['watch','server']);
