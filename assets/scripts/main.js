e78f42e8036d0c7452e66cc9c0969985e81381ac
simoneas02 <simoneas02@hotmail.com>
2017-01-21 01:25:24 -0300

    Update 2017-01-21T04:25:06.293Z

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log('ok');
+
+var forever = function forever(amor) {
+  console.log(amor + " stay with me");
+};
+
+forever("Afonso");
--- src/assets/scripts/main.js
+++
@@ -1,7 +0,0 @@
-console.log('ok');
-
-const forever = (amor) =>{
-  console.log(amor + " stay with me");
-}
-
-forever("Afonso")
